f853022d28ed2e9aa0413d194a8f80ee36779caf
Masato TSUTSUMI <masato.11.soccer@gmail.com>
2019-01-03 12:04:52 +0900

    init

---
+++ treasure.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void getEightByEightBoard()
+{
+}
+
+void proceed()
+{
+}
+
+int setTreasureAtRandomPlace()
+{
+}
+
+void returnToStart()
+{
+}
+
+void treasure()
+{
+}
+
+int main()
+{
+}
