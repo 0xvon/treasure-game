3f1a3bca7c2d88794ec261ad875a720835a1534a
Masato TSUTSUMI <masato.11.soccer@gmail.com>
2019-01-03 14:08:45 +0900

    fix

--- treasure.c
+++ treasure.c
@@ -7,6 +7,8 @@ int time = 0;
 int current[6][6];
 int countOfpossessingTreasure = 0;
 int countOfJudgedBox = 0;
+int row = 0;
+int column = 0;
 
 int setRandom(int row, int column)
 {
@@ -21,16 +23,43 @@ int setRandom(int row, int column)
         return 0;
     }
 }
+void judgeIsThereTreasure(row, column)
+{
+    if (board[row][column] == 1)
+    {
+        countOfpossessingTreasure++;
+    }
+
+    // if (countOfpossessingTreasure > 1)
+    // {
+    //     returnToStart(row, column);
+    // }
+    // else
+    // {
+    //     proceed();
+    // }
+}
 
 void proceed(row, column)
 {
     if (row < 6)
     {
-        current[row + 1][column] = 1;
+        row++;
+        current[row][column] = 1;
+        countOfJudgedBox++;
+
+        proceed(row, column);
     }
     else if (column < 6)
     {
-        current[row][column + 1] = 1
+        column++;
+        current[row][column] = 1;
+        countOfJudgedBox++;
+        proceed(row, column);
+    }
+    else
+    {
+        printf("掘りつくしました");
     }
     // TODO: row=5, column=5の場合の処理
 }
@@ -47,13 +76,16 @@ int setTreasureAtRandomPlace()
     }
 }
 
-void returnToStart()
+void returnToStart(row, column)
 {
+    int distance = row + column;
+    row = 0, column = 0;
 }
 
 void treasure()
 {
-    getEightByEightBoard();
+    current[0][0] = 1;
+    for (int i = 0; int i = 6;)
 }
 
 int main()
