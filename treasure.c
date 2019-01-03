f0915c5c831671a15da5c247c2796b771387ee53
Masato TSUTSUMI <masato.11.soccer@gmail.com>
2019-01-03 13:31:48 +0900

    proceed and set

--- treasure.c
+++ treasure.c
@@ -8,24 +8,43 @@ int current[6][6];
 int countOfpossessingTreasure = 0;
 int countOfJudgedBox = 0;
 
-void getEightByEightBoard()
+int setRandom(int row, int column)
+{
+    double random = (double)rand() / (RAND_MAX + 1.0);
+    double probability = (row + column) * 0.1;
+    if (random < probability)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+void proceed(row, column)
+{
+    if (row < 6)
+    {
+        current[row + 1][column] = 1;
+    }
+    else if (column < 6)
+    {
+        current[row][column + 1] = 1
+    }
+    // TODO: row=5, column=5の場合の処理
+}
+
+int setTreasureAtRandomPlace()
 {
     int board[6][6];
     for (int i = 0; i < 6; i++)
     {
         for (int k = 0; k < 6; k++)
         {
-            board[i][k] == 0;
+            board[i][k] == setRandom(i, k);
         }
     }
-}
-
-void proceed()
-{
-}
-
-int setTreasureAtRandomPlace()
-{
 }
 
 void returnToStart()
