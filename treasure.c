0f85a77964be196a6c963a089704b97e8788cfc1
Masato TSUTSUMI <masato.11.soccer@gmail.com>
2019-01-16 15:41:02 +0900

    time constraint

--- treasure.c
+++ treasure.c
@@ -2,11 +2,19 @@
 #include <stdio.h>
 #include <time.h>
 
+// 開拓数
 int countOfJudgedBox = 0;
+// 経過時間
 int passingTime = 0;
+// 設定時間
+int settingTime = 0;
+// 現在地・開拓位置
 int current[6][6];
+// ボード・お宝の有無
 int map[6][6];
+// 総数
 int countOfpossessingTreasure = 0;
+// 所有数
 int countOfpossessingTreasureForMe = 0;
 // srand(time(NULL) * 12345);
 // int row = 0;
@@ -44,6 +52,7 @@ void setTreasureAtRandomPlace()
     printf("\n");
     printf("\n");
 }
+
 void judgeIsThereTreasure(row, column)
 {
     if (map[row][column] == 1)
@@ -52,23 +61,36 @@ void judgeIsThereTreasure(row, column)
     }
 }
 
+void timeup()
+{
+    countOfpossessingTreasure += countOfpossessingTreasureForMe;
+    printf("時間切れです\n");
+    printf("経過時間t=%d, お宝の総数=%d\n", passingTime, countOfpossessingTreasure);
+    printf("終了します。また来てね！\n");
+    exit(0);
+}
+
 void returnToStart(row, column)
 {
     int distance = row + column;
     passingTime += (countOfpossessingTreasureForMe * distance);
     countOfpossessingTreasure += countOfpossessingTreasureForMe;
     countOfpossessingTreasureForMe = 0;
+    if (settingTime <= passingTime)
+    {
+        timeup();
+    }
 }
 
 int canIContinue()
 {
     if (countOfpossessingTreasureForMe > 1)
     {
-        return 1;
+        return 0;
     }
     else
     {
-        return 0;
+        return 1;
     }
 }
 
@@ -82,7 +104,11 @@ void proceed(row, column)
         current[row][column] = 1;
         countOfJudgedBox++;
         judgeIsThereTreasure(row, column);
-        if (canIContinue() == 1)
+        if (settingTime <= passingTime)
+        {
+            timeup();
+        }
+        if (canIContinue() == 0)
         {
             returnToStart(row, column);
         }
@@ -97,7 +123,11 @@ void proceed(row, column)
         current[row][column] = 1;
         countOfJudgedBox++;
         judgeIsThereTreasure(row, column);
-        if (canIContinue() == 1)
+        if (settingTime <= passingTime)
+        {
+            timeup();
+        }
+        if (canIContinue() == 0)
         {
             returnToStart(row, column);
         }
@@ -131,7 +161,6 @@ int judgeWhetherDeadOrAlive()
 void treasure()
 {
     current[0][0] = 1;
-    setTreasureAtRandomPlace();
     proceed(0, 0);
     if (judgeWhetherDeadOrAlive() == 1)
     {
@@ -141,6 +170,17 @@ void treasure()
 
 int main()
 {
+    setTreasureAtRandomPlace();
+    printf("設定時間\n>>");
+    scanf("%d", &settingTime);
+    if (settingTime < 0)
+    {
+        printf("正しい時間を設定してね");
+        main();
+    }
+    printf("スタート!!");
+    printf("\n");
+
     treasure();
 
     return 0;
